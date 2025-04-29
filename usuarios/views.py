241ddd8734f732c931b17d220ad1be564222ac17
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-04-29 00:26:31 -0300

    Criando os templates: base.html cadastrar_beneficiarios.html

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rolepermissions.decorators import has_permission_decorator
 
+
+@has_permission_decorator('cadastrar_beneficiarios')
 def cadastrar_beneficiarios(request):
-    return HttpResponse('Teste')
+    return render(request, 'cadastrar_beneficiarios.html')
