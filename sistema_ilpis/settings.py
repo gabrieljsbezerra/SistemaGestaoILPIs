b57d2bce546db5ae27b1feb8a15343e37223c2db
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-05-04 00:20:46 -0300

    Definindo as classes de erros do Messages

--- sistema_ilpis/settings.py
+++ sistema_ilpis/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,13 @@ AUTH_USER_MODEL = 'usuarios.Users'
 # Role Permissions
 
 ROLEPERMISSIONS_MODULE = 'sistema_ilpis.roles'
+
+# Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning'    
+}
