d622bfda5215bc11b48ddac66a3f84ab9523ba85
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-05-04 00:42:24 -0300

    - Ajustes de linguagem no settings - Retornos em pt-BR e na timezone do Brasil

--- sistema_ilpis/settings.py
+++ sistema_ilpis/settings.py
@@ -90,18 +90,18 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-BR"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -137,5 +137,5 @@ MESSAGE_TAGS = {
     constants.ERROR: 'alert-danger',
     constants.SUCCESS: 'alert-success',
     constants.INFO: 'alert-info',
-    constants.WARNING: 'alert-warning'    
+    constants.WARNING: 'alert-warning'
 }
