e9d9098f3393ca9496f86f239a239e2bb18ec091
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-05-04 01:09:10 -0300

    - Criando o novo app de estoque de remedios - ajustes nem settings, model e admin

---
+++ estoque/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Categoria(models.Model): #Categoria do remedio - foreign key
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
+    
+class Remedio(models.Model):
+    nome = models.CharField(max_length=60)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True) # 1-N
+    marca = models.CharField(max_length=40)
+    quantidade = models.FloatField()
+    preco_compra = models.FloatField()
+
+    def __str__(self):
+        return self.nome
