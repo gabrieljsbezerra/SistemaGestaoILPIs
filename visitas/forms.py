b9922eb4eabfd351488237903c14d73047add634
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-05-17 22:51:24 -0300

    - Crud de visitas funcionando perfeitamente

--- visitas/forms.py
+++ visitas/forms.py
@@ -5,3 +5,15 @@ class VisitaForm(forms.ModelForm):
     class Meta:
         model = Visita
         fields = ['responsavel', 'idoso', 'data', 'horario', 'observacoes']
+        widgets = {
+            'data': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}, format='%d-%m-%Y'),
+            'horario': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-select'}),
+            'idoso': forms.Select(attrs={'class': 'form-select'}),
+            'observacoes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Corrige exibição da data no campo do formulário (usado no modo edição)
+        if self.instance and self.instance.data:
+            self.initial['data'] = self.instance.data.strftime('%Y-%m-%d')
