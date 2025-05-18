562cd9200a95c418d275cf9cc0fe211455688172
Gabriel Joao <gabriel_jb30@hotmail.com>
2025-05-17 22:57:58 -0300

    buscas implementadas no app visitas para buscar na listagem

--- visitas/views.py
+++ visitas/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Visita
 from .forms import VisitaForm
+from django.db.models import Q
+from datetime import datetime
 
 def lista_visitas(request):
+    busca = request.GET.get('busca', '')
     visitas = Visita.objects.all()
-    return render(request, 'visitas/lista_visitas.html', {'visitas': visitas})
 
+    if busca:
+        filtros = (
+            Q(idoso__nome__icontains=busca) |
+            Q(responsavel__nome__icontains=busca) |
+            Q(observacoes__icontains=busca)
+        )
+
+        try:
+            data_busca = datetime.strptime(busca, "%d/%m/%Y").date()
+            filtros |= Q(data=data_busca)
+        except ValueError:
+            try:
+                data_busca = datetime.strptime(busca, "%Y-%m-%d").date()
+                filtros |= Q(data=data_busca)
+            except ValueError:
+                pass
+
+        visitas = visitas.filter(filtros)
+
+    return render(request, 'visitas/lista_visitas.html', {'visitas': visitas, 'busca': busca})
 def nova_visita(request):
     form = VisitaForm(request.POST or None)
     if form.is_valid():
